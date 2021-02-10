3a2577dcafcc6f5902b61ae43f11dd552390d501
Syed Tahseen <53108646+SyedTahseen@users.noreply.github.com>
2021-02-10 11:41:04 +0500

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -3,18 +3,18 @@ from tobrot.sample_config import Config
 #read readme too before filling these stuffs
 
 class Config(Config):
-    TG_BOT_TOKEN= "1636476117:AAHbfCPwmDO8GmkgjjEYJ6q2PVAis6sayr4" #imp
+    TG_BOT_TOKEN= "1658143488:AAHv7HKl9Tyz20VjrzBz1HkDrKQ3NKMnddg" #imp
     APP_ID = 1244657 #imp
     API_HASH = "b0fb3b798db670add65ae064006a856d" #imp
     AUTH_CHANNEL = [-1001405974567, 873537185] #imp replace your_id with your id from telegram or delete
     INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev"
-    GLEECH_COMMAND = "gleech@Mirroredxbot"
-    YTDL_COMMAND = 'ytdl@Mirroredxbot'
-    TELEGRAM_LEECH_COMMAND_G = "tleech@Mirroredxbot"
+    GLEECH_COMMAND = "gleech@TorrentUpBot"
+    YTDL_COMMAND = 'ytdl@TorrentUpBot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@TorrentUpBot"
     CLONE_COMMAND_G = "gclone@groupname"
     PYTDL_COMMAND_G = "pytdl@groupname"
-    DESTINATION_FOLDER = "Mirroredxbot"
-    LEECH_COMMAND = "leech@Mirroredxbot"
-    CANCEL_COMMAND_G = "cancel@Mirroredxbot"
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.-XX9fgU--","token_type":"Bearer","refresh_token":"1//0gLZEp8-VV2rZyourtCgYIARAAGBASNwF-","expiry":"2020-09-03T14:22:34.599776393Z"}\nteam_drive = """
+    DESTINATION_FOLDER = "TorrentUpBot"
+    LEECH_COMMAND = "leech@TorrentUpBot"
+    CANCEL_COMMAND_G = "cancel@TorrentUpBot"
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.A0AfH6SMC77_jG23q5CWp-tnvDuBOAvmaOMJOlam4pef7zVeLivIUVJvY_vrpsKvnrxefc5sYCMS0Q4D5cOccNEpYEwvSiYeOo4eKe9ru-T5cEj_aAyyQdl0Lek-QnnCMWj61TB_-eli0q0V6MEDXTBrHJovlE","token_type":"Bearer","refresh_token":"1//04J9D1WvLhkERCgYIARAAGAQSNwF-L9IrKMxyzsG96G0agfwWu7pTyssNyXu-0xUUINciWQKW1sZiJ1mAOv4kjbbyKQ8DsgIxP3E","expiry":"2021-02-10T14:32:49.972607642+07:00"}\nteam_drive = 0AP-celMwOXo9Uk9PVA"""
     #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
