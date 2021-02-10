802e3977f0a8f8a74750e3617f43f18da7e0c4b4
Syed Tahseen <53108646+SyedTahseen@users.noreply.github.com>
2021-02-10 11:36:52 +0500

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -5,21 +5,21 @@ from tobrot.sample_config import Config
 #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
 
 class Config(Config):
-    TG_BOT_TOKEN= "1636476117:AAHbfCPwmDO8GmkgjjEYJ6q2PVAis6sayr4"
+    TG_BOT_TOKEN= "1658143488:AAHv7HKl9Tyz20VjrzBz1HkDrKQ3NKMnddg"
     APP_ID = 1244657
     API_HASH = "b0fb3b798db670add65ae064006a856d"
     OWNER_ID = 873537185
     AUTH_CHANNEL = [-1001405974567]
-    DESTINATION_FOLDER = "Mirroredxbot" #Name of your folder read readme(not id of the folder)
+    DESTINATION_FOLDER = "TorrentUpBot" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     RCLONE_CONFIG = """
 [DRIVE]
 type = drive
-client_id = 28dslkgjsdl-fill-your-details-apps.googleusercontent.com
-client_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc
+client_id = 202264815644.apps.googleusercontent.com
+client_secret = X4Z3ca8xfWDb1Voo-F9a7ZxJ
 scope = drive
 root_folder_id =
-token = {"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczsHg6MYPSE6hXo-PJ-fill-your-details-s06KAecfw_H-tYThBtbs:20:25.430920315Z"}
-team_drive = 0AB0q-fill-your-details-sdrgsgsdUk9PVA
+token = {"access_token":"ya29.A0AfH6SMC77_jG23q5CWp-tnvDuBOAvmaOMJOlam4pef7zVeLivIUVJvY_vrpsKvnrxefc5sYCMS0Q4D5cOccNEpYEwvSiYeOo4eKe9ru-T5cEj_aAyyQdl0Lek-QnnCMWj61TB_-eli0q0V6MEDXTBrHJovlE","token_type":"Bearer","refresh_token":"1//04J9D1WvLhkERCgYIARAAGAQSNwF-L9IrKMxyzsG96G0agfwWu7pTyssNyXu-0xUUINciWQKW1sZiJ1mAOv4kjbbyKQ8DsgIxP3E","expiry":"2021-02-10T14:32:49.972607642+07:00"}
+team_drive = 0AP-celMwOXo9Uk9PVA
 """
